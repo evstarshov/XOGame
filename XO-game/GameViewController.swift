8e6fff4b1ce548b3a1534fd35ef53f26380264ea
EugeneStarshov <yeenzo@yandex.ru>
2022-01-14 13:40:04 +0300

    gotoFirstState & gotonextState funcs

--- XO-game/GameViewController.swift
+++ XO-game/GameViewController.swift
@@ -10,13 +10,46 @@ class GameViewController: UIViewController {
     @IBOutlet var winnerLabel: UILabel!
     @IBOutlet var restartButton: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        gameboardView.onSelectPosition = { [weak self] position in
-            guard let self = self else { return }
-            self.gameboardView.placeMarkView(XView(), at: position)
+    private let gameboard = Gameboard()
+    
+    private var currentState: GameState! {
+        didSet {
+            self.currentState.begin()
         }
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.goToFirstState()
+        
+        gameboardView.onSelectPosition = { [weak self] position in
+            guard let self = self else { return }
+            self.currentState.addMark(at: position)
+            if self.currentState.isCompleted {
+                self.goToNextState()
+            }
+        }
+    }
+    
+    private func goToFirstState() {
+        self.currentState = PlayerInputState(
+            player: .first,
+            gameViewController: self,
+            gameboard: gameboard,
+            gameboardView: gameboardView)
+    }
+    
+    private func goToNextState() {
+        if let playerInputState = currentState as? PlayerInputState {
+            self.currentState = PlayerInputState(
+                player: playerInputState.player.next,
+                gameViewController: self,
+                gameboard: gameboard,
+                gameboardView: gameboardView)
+        }
+    }
+    
+    // --- IBActions:
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
         
